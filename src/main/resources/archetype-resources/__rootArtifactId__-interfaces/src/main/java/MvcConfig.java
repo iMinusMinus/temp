2415f0e291ced438dd93ce660c5dcb893d5cea42
梁明 <liangming@cathay-ins.com.cn>
2020-03-22 18:29:55 +0800

    remove context from mvc; jpa refactor.

--- src/main/resources/archetype-resources/__rootArtifactId__-interfaces/src/main/java/MvcConfig.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-interfaces/src/main/java/MvcConfig.java
@@ -1,13 +1,17 @@
 package ${package};
 
+#if(!$configType.contains('xml'))
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+#end
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+#if(!$configType.contains('xml'))
 @Configuration
 @ComponentScan(value = "${package}")
 @EnableWebMvc
+#end
 public class MvcConfig extends WebMvcConfigurationSupport {
 
 }
