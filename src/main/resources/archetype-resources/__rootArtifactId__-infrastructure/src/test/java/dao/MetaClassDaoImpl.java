49910be4f34d769676a98d240f6b237feec04882
梁明 <liangming@cathay-ins.com.cn>
2020-04-04 17:10:33 +0800

    fix: specify value when multi transactionManager exist

--- src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/test/java/dao/MetaClassDaoImpl.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/test/java/dao/MetaClassDaoImpl.java
@@ -17,14 +17,14 @@ public class MetaClassDaoImpl /* extends HibernateDaoSupport */ implements MetaC
     private SessionFactory sessionFactory;
 
     @Override
-    @Transactional
+    @Transactional(value = "transactionManager")
     public long save(MetaClass domain) {
         sessionFactory.getCurrentSession().save(domain);## No Transaction, no session
         return domain.getId();
     }
 
     @Override
-    @Transactional
+    @Transactional(value = "transactionManager")
     public int delete(long id) {
         ## sessionFactory.getCurrentSession().delete(entity); 按id删除，但非空列需赋值；打印SQL为update meta_class set class_name = ? where class_name = ?
         sessionFactory.getCurrentSession().createQuery("delete MetaClass m where m.id = " + id).executeUpdate();
@@ -32,25 +32,25 @@ public class MetaClassDaoImpl /* extends HibernateDaoSupport */ implements MetaC
     }
 
     @Override
-    @Transactional
+    @Transactional(value = "transactionManager")
     public int update(MetaClass newValue, MetaClass oldValue) {
         return 0;
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional(value = "transactionManager", readOnly = true)
     public MetaClass findOne(MetaClass condition) {
         return null;
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional(value = "transactionManager", readOnly = true)
     public int count(MetaClass condition) {
         return 0;
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional(value = "transactionManager", readOnly = true)
     public List<MetaClass> query(MetaClass condition, int offset, int limit) {
         return null;
     }
