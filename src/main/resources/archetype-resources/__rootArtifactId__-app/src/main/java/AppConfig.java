63f75343a14e85f663186c680537b5500fb4c5ec
iMinusMinus <mean.leung@outlook.com>
2020-08-16 00:11:20 +0800

    dubboand hibernate related update

--- src/main/resources/archetype-resources/__rootArtifactId__-app/src/main/java/AppConfig.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-app/src/main/java/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 //@PropertySource(value = {"classpath:application.properties"})
-@ComponentScan(value = "${groupId}.${artifactId}")
+@ComponentScan(value = "${package}")
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
 public class AppConfig {
--- src/main/resources/archetype-resources/__rootArtifactId__-app/src/test/java/dao/MetaClassDaoImpl.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-app/src/test/java/dao/MetaClassDaoImpl.java
@@ -3,16 +3,21 @@ package ${package}.dao;
 import java.util.List;
 import javax.annotation.Resource;
 
+#if($framework.contains('hibernate'))
 import org.hibernate.SessionFactory;
+#end
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import ${package}.domain.MetaClass;
 
+#if($framework.contains('hibernate'))
 @Repository
-public class MetaClassDaoImpl /* extends HibernateDaoSupport */ implements MetaClassMapper {
+#end
+public class MetaClassDaoImpl /* extends HibernateDaoSupport */#if($framework.contains('hibernate')) implements MetaClassMapper#{end} {
 
+#if($framework.contains('hibernate'))
     @Resource
     private SessionFactory sessionFactory;
 
@@ -20,6 +25,7 @@ public class MetaClassDaoImpl /* extends HibernateDaoSupport */ implements MetaC
     @Transactional(value = "transactionManager")
     public long save(MetaClass domain) {
         sessionFactory.getCurrentSession().save(domain);## No Transaction, no session
+
         return domain.getId();
     }
 
@@ -27,6 +33,7 @@ public class MetaClassDaoImpl /* extends HibernateDaoSupport */ implements MetaC
     @Transactional(value = "transactionManager")
     public int delete(long id) {
         ## sessionFactory.getCurrentSession().delete(entity); 按id删除，但非空列需赋值；打印SQL为update meta_class set class_name = ? where class_name = ?
+
         sessionFactory.getCurrentSession().createQuery("delete MetaClass m where m.id = " + id).executeUpdate();
         return 0;
     }
@@ -54,4 +61,5 @@ public class MetaClassDaoImpl /* extends HibernateDaoSupport */ implements MetaC
     public List<MetaClass> query(MetaClass condition, int offset, int limit) {
         return null;
     }
+#end
 }
