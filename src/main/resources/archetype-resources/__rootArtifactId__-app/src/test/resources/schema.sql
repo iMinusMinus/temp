0e681aa7cfccbdd4ac5ab19aec71d83bbd479935
iMinusMinus <mean.leung@outlook.com>
2021-06-14 12:27:27 +0800

    rename 'proeprty-name' to 'property-key'.(refer Archaius JDBCConfigurationSource)

--- src/main/resources/archetype-resources/__rootArtifactId__-app/src/test/resources/schema.sql
+++ src/main/resources/archetype-resources/__rootArtifactId__-app/src/test/resources/schema.sql
@@ -6,4 +6,4 @@ CREATE TABLE meta_class (id BIGINT NOT NULL, class_name VARCHAR(65535), super_cl
 #if($showComment)
 -- refer: org.springframework.jdbc.core.support.JdbcBeanDefinitionReader
 #end
-CREATE TABLE properties_bean_definition (id BIGINT NOT NULL AUTO_INCREMENT, bean_name VARCHAR(64) NOT NULL, property_name VARCHAR(255) NOT NULL, property_value VARCHAR(2000), PRIMARY KEY (id));
+CREATE TABLE properties_bean_definition (id BIGINT NOT NULL AUTO_INCREMENT, bean_name VARCHAR(64) NOT NULL, property_key VARCHAR(255) NOT NULL, property_value VARCHAR(2000), PRIMARY KEY (id));
