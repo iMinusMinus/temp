91797af5ad9dd0b50fe9e1265d7190cc8cea18be
iMinusMinus <mean.leung@outlook.com>
2022-03-13 10:17:15 +0800

    add common field in jpa or spring-data-commons

--- src/main/resources/archetype-resources/__rootArtifactId__-app/src/test/java/domain/AbstractDomain.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-app/src/test/java/domain/AbstractDomain.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 #if($framework.contains("jpa") and $configType.contains("@"))
 import javax.persistence.GenerationType;
@@ -27,7 +28,20 @@ public abstract class AbstractDomain implements Serializable {
     @SequenceGenerator(name = "pg_meta_class_seq", sequenceName = "meta_class_id_seq")
     @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="pg_meta_class_seq")## SEQUENCE: PsotgreSQL/Oracle, IDENTITY: MySQL
 #end
-
     protected long id;
+
+## Refer: spring-data-commons @CreatedBy
+    protected String createdBy;
+
+## Refer: spring-data-commons @CreatedDate
+    protected LocalDateTime createdDate;
+
+## Refer: spring-data-commons @LastModifiedBy
+    protected String lastModifiedBy;
+
+## Refer: spring-data-commons @LastModifiedDate
+    protected LocalDateTime lastModifiedDate;
+
+    protected int version;
 
 }
